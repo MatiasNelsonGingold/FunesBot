b213392ea53fc8e5e7c7e2ac837a42e6a0e75791
Ian Meldrum <ianmeldrum97@gmail.com>
2023-06-26 23:35:14 -0500

    with notebook with FAQ relatable questions by user

---
+++ model_1/params.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+
+
+
+
+
+##################  CONSTANTS  #####################
+matias_api_key=os.environ.get("api_matias")
+api_matias='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'
---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+
+setup(name='model_1',
+      version="0.0.1",
+      description="first model using open_ai ",
+      license="MIT",
+      author="Ian Meldrum",
+      author_email="ianmeldrum97@gmail.com",
+      #url="https://github.com/MatiasNelsonGingold/FunesBot,
+      install_requires=requirements,
+      packages=find_packages(),
+      test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
