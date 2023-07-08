9fe3328796250adfde49c80faefc6e2c7f39de30
MatiasNelsonGingold <matias.nelsongingold@gmail.com>
2023-07-08 17:40:39 -0300

    New version!2

--- model_1/main.py
+++ model_1/main.py
@@ -6,28 +6,15 @@ from primer_input import *
 from predict import *
 
 #Primer paso es el preprocesamiento del libro
-<<<<<<< HEAD
-document_store=primer_input_func()
-
-#segundo paso es crear el pipe, crear el enlace con openai a través de la librería de langchain
-#Por último, crear el template del prompt que se utilizará
-pipe,chat,prompt_template =initialize_model(document_store)
-
-model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
-
-#llamar la pregunta del usuario
-respuesta_u=predict(query='Who is Pierre?',pipe=pipe,chat=chat,prompt_template=prompt_template)
-=======
 document_store_f=primer_input_func()
-
 
 #segundo paso es crear el pipe, crear el enlace con openai a través de la librería de langchain
 #Por último, crear el template del prompt que se utilizará
 pipe,chat,prompt_template =initialize_model(document_store_f)
 
+model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-dot-v1')
 
 #llamar la pregunta del usuario
-respuesta_u=predict(query='who is pierre?',pipe=pipe,chat=chat,prompt_template=prompt_template)
->>>>>>> main
+respuesta_u=predict(query='Who is Pierre?',pipe=pipe,chat=chat,prompt_template=prompt_template)
 
 print(respuesta_u)
